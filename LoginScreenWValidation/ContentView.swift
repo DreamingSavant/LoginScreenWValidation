e3621790d850347e18044d53eead7a7088b1a905
DreamingSavant <rodpwood@gmail.com>
2024-03-04 16:17:59 -0500

    Initial Commit

---
+++ LoginScreenWValidation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginScreenWValidation
+//
+//  Created by Roderick Presswood on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginScreenWValidation/LoginScreenWValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenWValidationApp.swift
+//  LoginScreenWValidation
+//
+//  Created by Roderick Presswood on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenWValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
